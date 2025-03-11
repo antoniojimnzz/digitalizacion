1099d27f28c2e42e7aba57391358182eec0e421a
antoniojimnzz <ajimrom0804@g.educaand.es>
2025-03-11 11:01:08 +0100

    Cambio 4

--- digitalizacion2.js
+++ digitalizacion2.js
@@ -7,6 +7,8 @@ function sumarCuadro(){
     numero_cuadros = numero_cuadros + 1;
     nuevoDiv.textContent = "Nuevo div " + numero_cuadros;
 
+    nuevoDiv.addEventListener ("click", sumarProducto )
+
     nuevoDiv.classList.add("cuadro")
 
     console.log(nuevoDiv)
@@ -15,4 +17,14 @@ function sumarCuadro(){
 for(let numero_cuadros = 0;numero_cuadros < 12;numero_cuadros = numero_cuadros + 1){
     sumarCuadro();
 }
+
+function sumarProducto(){
+    let nuevoProducto = document.createElement ("div");
+
+    nuevoProducto.classList.add (producto);
+
+    contenedor3.appendChild (nuevoProducto);
+}
+
+
 
